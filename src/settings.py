72c1fc79b4c577e570b978a94ddc0daa13bfcc4d
Radjin Sardjoe Missier <rmissier@gmail.com>
2026-02-16 00:41:14 +0100

    Add Spectrum settings and initialize directories in Settings class

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ class Settings:
     """Settings for the application."""
 
     work_dir: Path = Path.home() / "projects_data" / "external_order"
+    digitals_dir: Path = field(default=Path.home(), init=False)
     json_orders_dir: Path = field(default=Path.home(), init=False)
 
     # Harman settings
@@ -32,12 +33,18 @@ class Settings:
     odoo_rpc_user_id: int = int(os.getenv("ODOO_USER_ID", "0"))
     odoo_rpc_password: str = os.getenv("ODOO_PASSWORD", "")
 
+    # Spectrum settings
+    spectrum_base_url: str = os.getenv("SPECTRUM_BASE_URL", "")
+    spectrum_api_key: str = os.getenv("SPECTRUM_API_KEY", "")
+
     def __post_init__(self):
         """Initialize settings."""
         object.__setattr__(self, "harman_input_orders_dir", self.work_dir / "in" / "insdes")
+        object.__setattr__(self, "digitals_dir", self.work_dir / "digitals")
         object.__setattr__(self, "json_orders_dir", self.work_dir / "orders")
 
         self.harman_input_orders_dir.mkdir(parents=True, exist_ok=True)
+        self.digitals_dir.mkdir(parents=True, exist_ok=True)
         self.json_orders_dir.mkdir(parents=True, exist_ok=True)
 
 
