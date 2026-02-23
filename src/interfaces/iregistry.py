2a8a672148028550e9cc4961c7c717aa0f7aa380
Radjin Sardjoe Missier <rmissier@gmail.com>
2026-02-23 06:18:44 +0100

    Add IRegistry interface for storing and retrieving objects by name

--- src/interfaces/iregistry.py
+++ src/interfaces/iregistry.py
@@ -7,8 +7,6 @@ from typing import Protocol
 class IRegistry[T](Protocol):
     """A simple registry to store and retrieve objects by name."""
 
-    registry: dict[str, T]
-
     def register(self, name: str, obj: T) -> None:
         """Register an object with a given name."""
         ...
