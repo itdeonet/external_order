59345dde9c3dcd4c42c03a3508f500e6d8039aee
Radjin Sardjoe Missier <rmissier@gmail.com>
2026-02-23 06:14:07 +0100

    Add Config class for application settings with directory initialization

--- src/config.py
+++ src/config.py
@@ -11,22 +11,24 @@ load_dotenv()
 
 
 @dataclass(frozen=True, slots=True)
-class Settings:
+class Config:
     """Settings for the application."""
 
+    templates_dir: Path = Path(__file__).parent / "templates"
     work_dir: Path = Path.home() / "projects_data" / "external_order"
     digitals_dir: Path = field(default=Path.home(), init=False)
     json_orders_dir: Path = field(default=Path.home(), init=False)
     open_orders_dir: Path = field(default=Path.home(), init=False)
 
     # Harman settings
+    harman_input_orders_dir: Path = field(default=Path.home(), init=False)
+    harman_notify_dir: Path = field(default=Path.home(), init=False)
     harman_administration_id: int = 2
     harman_customer_id: int = 5380
     harman_pricelist_id: int = 2
     harman_order_provider: str = "Harman INSDES"
     harman_shipment_type: str = "harman%"
     harman_workdays_for_delivery: int = 2
-    harman_input_orders_dir: Path = field(default=Path.home(), init=False)
 
     # Odoo settings
     odoo_base_url: str = os.getenv("ODOO_BASE_URL", "")
@@ -40,18 +42,24 @@ class Settings:
 
     def __post_init__(self):
         """Initialize settings."""
-        object.__setattr__(self, "harman_input_orders_dir", self.work_dir / "in" / "insdes")
+        object.__setattr__(
+            self, "harman_input_orders_dir", self.work_dir / "harman" / "in" / "insdes"
+        )
+        object.__setattr__(self, "harman_notify_dir", self.work_dir / "harman" / "out")
         object.__setattr__(self, "digitals_dir", self.work_dir / "digitals")
         object.__setattr__(self, "json_orders_dir", self.work_dir / "orders")
         object.__setattr__(self, "open_orders_dir", self.work_dir / "open_orders")
 
         self.harman_input_orders_dir.mkdir(parents=True, exist_ok=True)
+        self.harman_notify_dir.mkdir(parents=True, exist_ok=True)
+        (self.harman_notify_dir / "desadvd96a").mkdir(parents=True, exist_ok=True)
+        (self.harman_notify_dir / "desadv99a").mkdir(parents=True, exist_ok=True)
         self.digitals_dir.mkdir(parents=True, exist_ok=True)
         self.json_orders_dir.mkdir(parents=True, exist_ok=True)
         self.open_orders_dir.mkdir(parents=True, exist_ok=True)
 
 
 @cache
-def get_settings() -> Settings:
+def get_config() -> Config:
     """Get the application settings."""
-    return Settings()
+    return Config()
